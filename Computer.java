f4de5d1108d7fc0dbdbdbb3d176aad8d8c6c4422
nithyajohny <109011787+nithyajohny@users.noreply.github.com>
2022-07-28 22:31:13 +0530

    Add files via upload

---
+++ Computer.java
@@ -0,0 +1,11 @@
+package org.system;
+
+public class Computer extends Desktop{
+
+	public void computermodel()
+	{
+		System.out.println("model is hp 15");
+	}
+}
+
+
---
+++ Desktop.java
@@ -0,0 +1,15 @@
+package org.system;
+
+public class Desktop 
+{
+	public void DesktopSize()
+	{
+		System.out.println("size is 25 inch");
+	}
+	public static void main(String[] args)
+	{
+    Computer c=new Computer();
+    c.computermodel();
+    c.DesktopSize();
+	}
+}
